784ff13113645c56ec30f224bdec732e6beaa906
carlos <carlos.mora.ram@gmail.com>
2021-01-13 15:37:47 -0500

    Dimular cambios en el input

--- src/components.js/AddCategory.js
+++ src/components.js/AddCategory.js
@@ -5,6 +5,7 @@ export const AddCategory = ({setCategories}) => {
    const [inputValue, setInputValue] = useState('');
    const handleInputChange = e =>{
       setInputValue(e.target.value);
+      console.log('habdle input llamado');
    }
    const handleSubmit = (e)=>{
       e.preventDefault();
--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -6,9 +6,20 @@ import { AddCategory } from '../../components.js/addCategory';
 
 describe('Test <AddCategory />', () => {
    const setCategories = ()=>{}
+   const wrapper = shallow(<AddCategory setCategories = {setCategories}/>);
    test('Debe mostrarse correctamente', () => {
-      const wrapper = shallow(<AddCategory setCategories = {setCategories}/>);
       expect(wrapper).toMatchSnapshot();
-   })
+   });
+   
+   test('Debe cambiar el input', () => {
+      const input = wrapper.find('input');
+      const value = 'Hola mundo';
+      input.simulate('change',{
+         target:{
+            value
+         }
+      });
+      console.log(input.html());
       
+   });
 })
